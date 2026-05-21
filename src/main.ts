d46bcd4a81f92df10c2f76af4f3afb26be56a5c2
Lohanna <lohannabt@gmail.com>
2026-05-20 21:53:55 -0300

    feat: adiciona documentacao swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <-- Adicionado
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
     origin: '*',
     credentials: true,
   });
+
+   const config = new DocumentBuilder()
+    .setTitle('VittaFit API')
+    .setDescription('Documentação das rotas do backend VittaFit')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 4000);
 }
